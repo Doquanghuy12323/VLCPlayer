a356728e9fc88ae0856bf6224db5e9173165e3fa
Doquanghuy12323 <eotoanpro@gmail.com>
2026-04-01 14:05:26 +0700

    fix: rewrite showHandyGuide with single-line string to fix unclosed literal

--- app/src/main/java/com/vlcplayer/app/MainActivity.java
+++ app/src/main/java/com/vlcplayer/app/MainActivity.java
@@ -429,28 +429,6 @@ public class MainActivity extends AppCompatActivity
             .setNegativeButton("Huy", null).show();
     }
 
-    private void showHandyGuide() {
-        new androidx.appcompat.app.AlertDialog.Builder(this)
-            .setTitle("Huong dan The Handy")
-            .setMessage("1. Ket noi The Handy vao WiFi
-
-"
-                + "2. Vao handyfeeling.com/setup tren trinh duyet
-
-"
-                + "3. Copy Connection Key hien thi tren trang do
-
-"
-                + "4. Dan key vao muc Nhap Connection Key trong app
-
-"
-                + "5. Mo video, bam nut Handy trong man hinh phat video
-
-"
-                + "6. Bam Ket noi roi Load Script de bat dau dong bo")
-            .setPositiveButton("OK", null).show();
-    }
-
 private void showLanguageDialog() {
         String[][] langs = AppLanguageManager.LANGUAGES;
         String[] names = new String[langs.length];
