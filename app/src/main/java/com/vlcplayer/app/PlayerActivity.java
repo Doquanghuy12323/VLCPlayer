e9170dd04a6c1702a6cc68a4f1d707bdf9b223dd
Doquanghuy12323 <eotoanpro@gmail.com>
2026-04-14 06:35:33 +0700

    fix: remove 2 extra closing braces before onStop

--- app/src/main/java/com/vlcplayer/app/PlayerActivity.java
+++ app/src/main/java/com/vlcplayer/app/PlayerActivity.java
@@ -969,8 +969,7 @@ public class PlayerActivity extends AppCompatActivity {
             }
         });
     }
-    }
-}
+    
 @Override protected void onStop() {
         super.onStop();
         saveHistory();
