1d4ed7dede4f35a2735dc1094a8eba4b685f6c0f
Doquanghuy12323 <eotoanpro@gmail.com>
2026-04-18 06:43:18 +0700

    fix: detach+reattach after stop() to clear old frame before new video

--- app/src/main/java/com/vlcplayer/app/PlayerActivity.java
+++ app/src/main/java/com/vlcplayer/app/PlayerActivity.java
@@ -497,6 +497,8 @@ public class PlayerActivity extends AppCompatActivity {
                     media.addOption(":file-caching=1500");
                     media.addOption(":codec=mediacodec_ndk,mediacodec,omxil,any");
                 mediaPlayer.stop();
+                mediaPlayer.detachViews();
+                mediaPlayer.attachViews(videoLayout, null, false, false);
                 mediaPlayer.setMedia(media);
                     media.release();
                     mediaPlayer.play();
